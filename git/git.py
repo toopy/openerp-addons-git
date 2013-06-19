df7b6743d35a18ad2e1708cd2ce671788609c56a
florent <florent@toopy.org>
2013-06-19 16:09:27 +0200

    Prepares commit update/delete mechanisms.

--- git/git.py
+++ git/git.py
@@ -1,5 +1,12 @@
+import os
+
 from osv import osv
 from osv import fields
+
+import openerp
+
+from pygit2 import GIT_SORT_TIME
+from pygit2 import Repository
 
 
 class git_repositories(osv.osv):
@@ -10,13 +17,64 @@ class git_repositories(osv.osv):
         'name': fields.char('Name', size=64, required=True, select=True),
     }
 
+    @property
+    def root(self):
+        _root = openerp.tools.config.get('git_root')
+        if not _root:
+            raise osv.except_osv('Root Config', 'Not set.')
+        if not os.path.exists(_root):
+            raise osv.except_osv('Root Path not Exist', _root)
+        return _root
+
+    def get_repo_path(self, repo_name):
+        path = os.path.join(self.root, repo_name, '.git')
+        if not os.path.exists(path):
+            raise osv.except_osv('Repo Path not Exist', path)
+        return path
+
+    def get_repo(self, repo_name):
+        return Repository(self.get_repo_path(repo_name)) 
+
+    def get_commits(self, repo_name):
+        repo = self.get_repo(repo_name)
+        for commit in repo.walk(repo.head.oid, GIT_SORT_TIME):
+            yield commit
+
+    def update_commits(self, cr, uid, repo):
+        for commit in self.get_commits(repo.name):
+            # TODO get previous commit in db
+            # TODO pass if in db
+            # TODO insert new commit in db
+            pass
+
+    def delete_commits(self, cr, uid, repo_id):
+        # TODO delete all
+        # _git_commit = self.pool.get('git_commit')
+        # ids = _git_commit.search(cr, uid, [('repo_id', '=', repo_id)])
+        # _git_commit.unlink(cr, uid, ids)
+        pass
+
     def create(self, cr, uid, values, context=None):
-        return super(git_repositories, self).create(cr, uid, values,
-                                               context=context)
+        _id = super(git_repositories, self).create(cr, uid, values,
+                                                   context=context)
+        repo = self.browse(cr, uid, _id)
+        self.update_commits(cr, uid, repo)
+        return _id
 
     def write(self, cr, uid, ids, values, context=None):
-        return super(git_repositories, self).write(cr, uid, ids, values,
-                                              context=context)
+        ids = super(git_repositories, self).write(cr, uid, ids, values,
+                                                  context=context)
+        # ensures ids list
+        ids = ids if isinstance(ids, list) else [ids]
+        # update commits for the given repositories
+        for repo in self.browse(cr, uid, ids):
+            self.update_commits(cr, uid, repo)
+        return ids
+
+    def unlink(cr, uid, ids, context=None):
+        self.delete_commits(cr, uid, ids)
+        return super(git_repositories, self).unlink(cr, uid, ids, values,
+                                                    context=context)
 
 
 git_repositories()
